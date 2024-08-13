2e281f31df49048d6cbb620e2442e4f9e501c472
swh9534@gmail.com <swh9534@gmail.com>
2024-08-13 16:28:40 +0900

    image clustering 수정

--- image_clustering.py
+++ image_clustering.py
@@ -1,6 +1,6 @@
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 import pandas as pd
 from sklearn.cluster import KMeans
@@ -9,9 +9,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import io
 import base64
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from util.pg_db_util import get_pg_connection
 
 app = FastAPI()
+
+# 설정: Jinja2 템플릿 디렉토리
+templates = Jinja2Templates(directory="templates")
+
+# 선택적: 정적 파일 서빙 (예: CSS, JS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def fetch_embedding_list(conn):
     query = "SELECT embedding FROM image_vector"
@@ -22,7 +30,7 @@ def perform_clustering(vectors, n_clusters):
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
     return kmeans.fit_predict(vectors)
 
-def reduce_dimensions(vectors, n_components=3):
+def reduce_dimensions(vectors, n_components=2):
     reducer = umap.UMAP(n_components=n_components, random_state=42)
     return reducer.fit_transform(vectors)
 
@@ -39,14 +47,14 @@ class ClusterRequest(BaseModel):
     n_clusters: int
 
 @app.post("/clustering")
-def cluster_and_visualize(request: ClusterRequest):
+def cluster_and_visualize(request: ClusterRequest, req: Request):
     try:
         conn, tunnel = get_pg_connection()
         vectors = fetch_embedding_list(conn)
         clusters = perform_clustering(vectors, request.n_clusters)
         vectors_2d = reduce_dimensions(vectors)
         img_str = visualize_clusters(vectors_2d, clusters)
-        return {"image": img_str}
+        return templates.TemplateResponse("result.html", {"request": req, "image": img_str})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:
